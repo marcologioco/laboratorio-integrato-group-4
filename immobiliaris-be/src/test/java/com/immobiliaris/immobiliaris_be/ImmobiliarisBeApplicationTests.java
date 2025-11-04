f24439cc8098ad3a0a5b67a8044037ff428a18ad
tommaferre <tommaso.ferrero@edu-its.it>
2025-11-04 21:32:58 +0100

    feat: Initialize Spring Boot project with basic structure and dependencies
    - Added pom.xml for Maven configuration with Spring Boot dependencies.
    - Created main application class ImmobiliarisBeApplication.
    - Configured application properties for H2 database and JPA.
    - Added SQL scripts for database schema and initial data.
    - Included custom CSS for styling the application.

---
+++ immobiliaris-be/src/test/java/com/immobiliaris/immobiliaris_be/ImmobiliarisBeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.immobiliaris.immobiliaris_be;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ImmobiliarisBeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
